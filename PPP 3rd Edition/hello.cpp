ccb63c68e2eb24a5ccb570f0ae66c4551bb542b6
B. Loloa <98461273+johnniedeppofficiel@users.noreply.github.com>
2026-05-03 01:25:10 +1000

    organising into exercises per book

---
+++ PPP 3rd Edition/hello.cpp
@@ -0,0 +1,17 @@
+/* NOTE: code within the comment block is for pre C++ 20.
+
+
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+*/
+import std;
+int main{
+    std::cout<<"Hello, World!\n";
+    
+}
